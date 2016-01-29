c61a8807bfd3b44f0cbd88bde116df051df0cf9e
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-01-29 17:23:08 +0100

    tslint fixes

--- LineUpView.ts
+++ LineUpView.ts
@@ -3,19 +3,18 @@
  */
 /// <reference path="./tsd.d.ts" />
 
-import C = require('../caleydo_core/main');
 import prov = require('../caleydo_provenance/main');
 import {AView, EViewMode} from './View';
 import lineup = require('lineupjs');
 import d3 = require('d3');
 
-export function numberCol(col:string, rows: any[], label = col) {
+export function numberCol(col:string, rows:any[], label = col) {
   return {
     type: 'number',
     column: col,
     label: col,
     domain: d3.extent(rows, (d) => d[col])
-  }
+  };
 }
 
 export function stringCol(col:string, label = col) {
@@ -23,7 +22,7 @@ export function stringCol(col:string, label = col) {
     type: 'string',
     column: col,
     label: col
-  }
+  };
 }
 
 export class ALineUpView extends AView {
@@ -46,10 +45,10 @@ export class ALineUpView extends AView {
     super.modeChanged(mode);
     if (this.lineup) {
       //collapse all columns
-      const compress = mode != EViewMode.FOCUS;
+      const compress = mode !== EViewMode.FOCUS;
       this.lineup.data.getRankings().forEach((r) => {
-        r.children.forEach((col,i) => {
-          if (i>0) {
+        r.children.forEach((col, i) => {
+          if (i > 0) {
             col.compressed = compress;
           }
         });
@@ -68,13 +67,13 @@ export class LineUpView extends ALineUpView {
 
   private build() {
     //generate random data
-    const rows = d3.range(300).map((i) => ({ name: 'Row#'+i, v1: Math.random()*100, v2: Math.random()*100}));
+    const rows = d3.range(300).map((i) => ({name: 'Row#' + i, v1: Math.random() * 100, v2: Math.random() * 100}));
 
     const columns = [stringCol('name'), numberCol('v1', rows), numberCol('v2', rows)];
     const l = this.buildLineUp(rows, columns);
     const r = l.data.pushRanking();
-    l.data.push(r,columns[0]).setWidth(130);
-    const stack = l.data.push(r,lineup.model.createStackDesc('Combined'));
+    l.data.push(r, columns[0]).setWidth(130);
+    const stack = l.data.push(r, lineup.model.createStackDesc('Combined'));
     stack.push(l.data.create(columns[1]));
     stack.push(l.data.create(columns[2]));
     l.update();

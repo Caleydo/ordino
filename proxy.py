67a4422f1bcbb91c98b8edea7788a7f4deffd47d
Holger Stitz <holger.stitz@jku.at>
2016-07-22 14:11:50 +0200

    Add logs to proxy.py. Caleydo/targid2#68

--- proxy.py
+++ proxy.py
@@ -17,10 +17,15 @@ def _to_site_url(site):
 
 @app.route('/<site>')
 def get_details(site):
+  import logging
+  _log = logging.getLogger(__name__)
+
   url = _to_site_url(site)
   if url:
+    _log.info('proxy request url: %s', url)
     print url
     r = requests.get(url)
+    _log.info('proxy response status code: %s', r.status_code)
     return Response(r.text, status=r.status_code, content_type=r.headers['content-type'])
   abort(404)
 

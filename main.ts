5d522fa17830ff3d13e25a39e1713520e21ffde8
Holger Stitz <holger.stitz@jku.at>
2016-07-28 16:02:55 +0200

    Hide provenance graph by default Closes Caleydo/targid2#121

--- main.ts
+++ main.ts
@@ -34,7 +34,8 @@ const elems = template.create(document.body, {
   }),
   application: '/targid2', // URL
   id: 'targid2',
-  recordSelectionTypes: null // no automatic selection recording
+  recordSelectionTypes: null, // no automatic selection recording
+  provVisCollapsed: true
 });
 
 // copy nodes from original document to new document (template)

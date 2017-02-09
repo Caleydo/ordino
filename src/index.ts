e2664ef55590f3c1ba7396cb5b470419adaeb273
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-09 09:56:56 +0100

    enable show report bug link

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ const elems = template.create(document.body, {
   provVisCollapsed: true,
   thumbnails: false,
   headerOptions: {
-    showReportBugLink: false
+    showReportBugLink: true
   }
 });
 

e052e2e72a63958cc1b1c0c8bec77ca2897603f4
Christian Lehner <christian.lehner@datavisyn.io>
2017-02-22 18:07:02 +0100

    added interface

--- src/GeneIDTypeDetector.ts
+++ src/GeneIDTypeDetector.ts
@@ -1,31 +1,33 @@
-function detectIDType(data: any[], accessor: (row: any) => string, sampleSize: number) : Promise<number> {
-  return new Promise((resolve) => {
-    const testSize = Math.min(data.length, sampleSize);
-    if (testSize <= 0) {
-      return 0;
-    }
-
-    let foundIDTypes = 0;
-    let validSize = 0;
-
-    for(let i = 0; i < testSize; ++i) {
-      const v = accessor(data[i]);
-
-      if (v == null || v.trim().length === 0) {
-        continue; //skip empty samples
-      }
-
-      if(v.indexOf('ENSG') >= 0) {
-        ++foundIDTypes;
-      }
-      ++validSize;
-    }
-
-    resolve(foundIDTypes / validSize);
-  });
+export interface IIDTypeDetector {
+  detectIDType: (data: any[], accessor: (row: any) => string, sampleSize: number) => Promise<number>|number;
 }
 
-export function geneIDTypeDetector() {
+function detectIDType(data: any[], accessor: (row: any) => string, sampleSize: number) : number {
+  const testSize = Math.min(data.length, sampleSize);
+  if (testSize <= 0) {
+    return 0;
+  }
+
+  let foundIDTypes = 0;
+  let validSize = 0;
+
+  for(let i = 0; i < testSize; ++i) {
+    const v = accessor(data[i]);
+
+    if (v == null || v.trim().length === 0) {
+      continue; //skip empty samples
+    }
+
+    if(v.indexOf('ENSG') >= 0) {
+      ++foundIDTypes;
+    }
+    ++validSize;
+  }
+
+  return foundIDTypes / validSize;
+}
+
+export function geneIDTypeDetector(): IIDTypeDetector {
   return {
     detectIDType
   };

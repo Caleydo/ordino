23e76e8d22b52aa72f1601f6ab195c525d6a584e
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-03-13 16:20:51 +0100

    add simple cached helper

---
+++ src/cached.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by Samuel Gratzl on 13.03.2017.
+ */
+
+
+const values = new Map<string, any>();
+
+/**
+ * simple delayed caching approach, you hand in the creator functions that is optionally being called
+ * @param key key to store
+ * @param creator the function to create in case the values not yet cached
+ * @return {any}
+ */
+export default function cached<T>(key: string, creator: () => T) {
+  if (values.has(key)) {
+    return values.get(key);
+  }
+  const v = creator();
+  values.set(key, v);
+  return v;
+}

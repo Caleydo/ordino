489a58b553cb5f8a8e45ae96672dc19169ff0541
Christian Lehner <christian.lehner@datavisyn.io>
2017-05-24 16:21:03 +0200

    included feedback

--- src/form/internal/FormButton.ts
+++ src/form/internal/FormButton.ts
@@ -3,8 +3,8 @@ import * as d3 from 'd3';
 import {EventHandler} from 'phovea_core/src/event';
 
 export interface IButtonElementDesc extends IFormElementDesc {
-  iconClasses: string;
   onClick: () => void;
+  iconClass?: string;
 }
 
 export default class FormButton extends EventHandler implements IFormElement {
@@ -30,20 +30,22 @@ export default class FormButton extends EventHandler implements IFormElement {
     this.$node.classed('hidden', !visible);
   }
 
-  get value() {
+  get value(): boolean {
     return this.clicked;
   }
 
-  set value(clicked) {
+  set value(clicked: boolean) {
     this.clicked = clicked;
   }
 
   build() {
     this.$button = this.$node.append('button').classed(this.desc.attributes.clazz, true);
-    this.$button.html(() => this.desc.iconClasses? `<i class="${this.desc.iconClasses}"></i> ${this.desc.label}` : this.desc.label);
+    this.$button.html(() => this.desc.iconClass? `<i class="${this.desc.iconClass}"></i> ${this.desc.label}` : this.desc.label);
     this.$button.on('click', () => {
       this.value = true;
       this.desc.onClick();
+      (<Event>d3.event).preventDefault();
+      (<Event>d3.event).stopPropagation();
     });
   }
 }

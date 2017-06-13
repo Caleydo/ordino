3d0ce1bafaf131095c526132b543937fb9c57c5e
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-06-13 13:07:33 +0200

    use big fat dialog for not found

--- src/Dialogs.ts
+++ src/Dialogs.ts
@@ -67,31 +67,32 @@ export function showErrorModalDialog(error: any) {
 
 
 export function showProveanceGraphNotFoundDialog(manager: CLUEGraphManager, id: string) {
-    const msg = `
-      <div class="alert alert-danger fade in">
-        <p>
-            <strong>Session Not Found!</strong> The requested session "${id}" was not found or is not accessible.
-        </p> 
-        <p>
-            Possible reasons are that you 
-            <ul>
-                <li>requested a <i>temporary session</i> that is already expired</li>
-                <li>tried to access a <i>temporary session</i> of another user</li>
-                <li>tried to access a <i>private persistent session</i> of another user</li>
-            </ul>
-        </p>
-        <p>
-            In the latter two cases, please contact the original owner of the session to create a public persistent session.     
-        </p>
-        <p>
-            <a href="#" class="alert-link" id="new-session-link">Click here to start a new temporary session</a>    
-        </p>
-    </div>`;
-  document.body.insertAdjacentHTML('beforeend', msg);
-  document.querySelector('#new-session-link').addEventListener('click', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    manager.newGraph();
+  const dialog = generateDialog('Session Not Found!', 'Create New Temporary Session');
+  // append bg-danger to the dialog parent element
+  dialog.body.parentElement.parentElement.parentElement.classList.add('bg-danger');
+  dialog.body.innerHTML = `
+      <p>
+          The requested session <strong>"${id}"</strong> was not found or is not accessible.
+      </p> 
+      <p>
+          Possible reasons are that you 
+          <ul>
+              <li>requested a <i>temporary session</i> that is already expired</li>
+              <li>tried to access a <i>temporary session</i> of another user</li>
+              <li>tried to access a <i>private persistent session</i> of another user</li>
+          </ul>
+      </p>
+      <p>
+          In the latter two cases, please contact the original owner of the session to create a public persistent session.     
+      </p>`;
+  dialog.onSubmit(() => {
+    dialog.hide();
     return false;
   });
+  dialog.onHide(() => {
+    dialog.destroy();
+    manager.newGraph();
+  });
+
+  dialog.show();
 }

44fe17eb17cd44637a76e645dcc750f4d8d8a39a
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-07-14 17:34:59 +0200

    improve caching behavior  * cache-busting for the main bundles (app, style, common)  * hash bashed names for chunks  * optimize caching setting in nginx

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@
 
 // Determine the order of css files manually
 
-import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
+import 'file-loader?name=index.html!extract-loader!html-loader?interpolate!ordino/src/index.html';
 import 'file-loader?name=404.html!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
 import 'phovea_ui/src/_bootstrap';

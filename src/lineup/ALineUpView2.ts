0ec50af54cc2359febfb72d63aece20717746879
Christian Lehner <christian.lehner@datavisyn.io>
2017-07-21 13:56:52 +0200

    fix duplicate color issue

--- src/lineup/ALineUpView2.ts
+++ src/lineup/ALineUpView2.ts
@@ -249,7 +249,7 @@ export abstract class ALineUpView2 extends AView {
     if(color) {
       this.colorMap.delete(id);
       if(this.colors.indexOf(color) === -1) {
-        this.colors.unshift(color);
+        this.colors.push(color);
       }
     }
   }
@@ -552,7 +552,10 @@ export abstract class ALineUpView2 extends AView {
     const rows = await this.loadRows();
     this.initRows(rows);
     this.initializedLineUp();
-    this.colors = this.getAvailableColumnColors();
+
+    if(!this.colors) {
+      this.colors = this.getAvailableColumnColors();
+    }
     this.setBusy(false);
   }
 

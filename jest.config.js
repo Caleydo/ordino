2d58563d9437441eb292b8736b2ab62d994c6dbd
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-03-20 09:30:13 +0100

    unify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 // test dependencies that require transformation
-const pluginsToTransform = [
-  'tdp_core'
+let pluginsToTransform = [
+  'tdp_*',
+  'phovea_*',
+  'lineupjs'
 ].join('|');
+
+if(pluginsToTransform.length > 0) {
+ /**  Attention: Negative Lookahead! This regex adds the specified repos to a whitelist that holds plugins that are excluded from the transformIgnorePatterns.
+  * This means that pluginsToTransform should contain all repos that export ts files. They can only be handled by the transformation. */
+    pluginsToTransform = `(?!${pluginsToTransform})`;
+}
 
 /**
  * TODO check if we can process inline webpack loaders (e.g. as found in https://github.com/phovea/phovea_ui/blob/master/src/_bootstrap.ts)
@@ -13,7 +21,6 @@ module.exports = {
     "\\.xml$": "jest-raw-loader"
   },
   testRegex: "(.*(test|spec))\\.(tsx?)$",
-  testURL: "http://localhost/",
   moduleFileExtensions: [
     "ts",
     "tsx",
@@ -24,12 +31,19 @@ module.exports = {
   ],
   modulePaths: [
     "src",
+    "../node_modules",
     "../"
   ],
-  transformIgnorePatterns: [`../node_modules/(?!${pluginsToTransform})`],
+  transformIgnorePatterns: [`../node_modules/${pluginsToTransform}`, `node_modules/${pluginsToTransform}`],
   globals: {
     "__VERSION__": "TEST_VERSION",
-    "__APP_CONTEXT__": "TEST_CONTEXT"
+    "__APP_CONTEXT__": "TEST_CONTEXT",
+    'ts-jest': {
+      // has to be set to true, otherwise i18n import fails
+      "tsConfig": {
+        "esModuleInterop": true,
+      }
+    }
   },
   moduleNameMapper: {
     "^.+\\.(css|less|scss|sass)$": "identity-obj-proxy",

24d9ad11878a9ff2dbd092046d9650c0ec9db768
Holger Stitz <holger.stitz@datavisyn.io>
2020-11-13 15:32:49 +0100

    Remove legacy index.html import in initialize.ts
    The file is generated automatically by webpack

--- dist/initialize.js
+++ dist/initialize.js
@@ -5,8 +5,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  ********************************************************************/
-// Determine the order of css files manually
-import './templates/index.html';
 import './templates/404.html';
 import './robots.txt';
 import './scss/main.scss';

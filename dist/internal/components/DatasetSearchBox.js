27ed689a98a18564fccadb8eaa33475ddfad0e2f
oltionchampari <oltion.champari@datavisyn.io>
2021-02-11 12:40:05 +0100

    Minor change

--- dist/internal/components/DatasetSearchBox.js
+++ dist/internal/components/DatasetSearchBox.js
@@ -3,9 +3,15 @@ import Select from 'react-select';
 import { Button, Col, Row } from 'react-bootstrap';
 export function DatasetSearchBox({}) {
     const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
+        { value: 'ABCD3', label: 'ABCD3' },
+        { value: 'ABCD1', label: 'ABCF3' },
+        { value: 'ABCD2', label: 'ABCD2' },
+        { value: 'ABCD4', label: 'ABCF1' },
+        { value: 'ABCD5', label: 'ABCD3' },
+        { value: 'ABCD6', label: 'ABCG3' },
+        { value: 'ABCD7', label: 'ABCG2' },
+        { value: 'ABCD8', label: 'ABCH3' },
+        { value: 'ABCD9', label: 'ABCM3' },
     ];
     return (React.createElement(Row, null,
         React.createElement(Col, null,

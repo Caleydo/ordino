f209bb61b1abffa40db79396de4634b7678723ef
oltionchampari <oltion.champari@datavisyn.io>
2021-03-05 10:14:04 +0100

    Refactor

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,13 @@
-export interface IOrdinoAppOptions {
+interface IHeaderOptions {
+    color: 'black' | 'white';
+    size: 'sm' | 'md' | 'lg';
+}
+
+export interface IAppOptions {
     enableDatasetsTab?: boolean;
     enableToursTab?: boolean;
     enableSessionsTab?: boolean;
+    enableMoreTab?: boolean;
+    header: IHeaderOptions;
 }
+

cc0a6765aefbfbab261a706e40dadd00a53d33c5
Holger Stitz <holger.stitz@datavisyn.io>
2021-03-31 18:17:20 +0200

    Remove navbar fixed and change overflow behavior
    The result is that the start menu itself is scrollable and not the whole page.

--- dist/internal/menu/StartMenuReact.js
+++ dist/internal/menu/StartMenuReact.js
@@ -51,7 +51,6 @@ export function StartMenuComponent({ header, mode, open }) {
     React.useEffect(() => {
         // switch header to dark theme when a tab is active
         header.toggleDarkTheme((activeTab) ? true : false);
-        header.togglePositionFixed((activeTab) ? true : false);
     }, [header, activeTab]);
     return (React.createElement(React.Fragment, null,
         ReactDOM.createPortal(React.createElement(MainMenuLinks, { tabs: tabs, activeTab: activeTab, setActiveTab: (a) => setActiveTab(a), mode: mode }), header.mainMenu),

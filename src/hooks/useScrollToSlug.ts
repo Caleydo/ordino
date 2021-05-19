3e073f1e484b699dda5edad709b5743b0c8e5159
Holger Stitz <holger.stitz@datavisyn.io>
2021-05-19 14:13:56 +0200

    Add slug typings

--- src/hooks/useScrollToSlug.ts
+++ src/hooks/useScrollToSlug.ts
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import {useParams} from 'react-router-dom';
 
+interface IParamTypes {
+  slug: string;
+}
+
 /**
  * Reads the slug of a route and scrolls to an element with this id,
  * if available on the page.
@@ -8,7 +12,7 @@ import {useParams} from 'react-router-dom';
  * @see https://github.com/rafgraph/react-router-hash-link/issues/13
  */
 export function useScrollToSlug() {
-  const {slug} = useParams();
+  const {slug}: IParamTypes = useParams();
 
   React.useEffect(() => {
     setTimeout(() => { // time out necessary for some browsers (e.g., Safari)

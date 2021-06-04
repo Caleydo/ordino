074dfbcf90e7a459b44ba6ed4a51a5dd1953c282
Holger Stitz <holger.stitz@datavisyn.io>
2021-06-04 18:16:17 +0200

    Semi-transparent start menu in overlay mode
    Closes Caleydo/tdp_bi_bioinfodb#1242

--- dist/internal/menu/StartMenu.js
+++ dist/internal/menu/StartMenu.js
@@ -123,7 +123,7 @@ function StartMenuTabWrapper(props) {
         return null;
     }
     return (React.createElement(React.Fragment, null, props.status === 'success' &&
-        React.createElement("div", { id: "ordino-start-menu", className: `ordino-start-menu tab-content ${props.activeTab ? 'ordino-start-menu-open' : ''}` }, props.tabs.map((tab) => (React.createElement("div", { className: `tab-pane fade ${props.activeTab === tab ? `active show` : ''} ${props.mode === EStartMenuMode.START ? `pt-5` : ''}`, key: tab.desc.id, id: tab.desc.id, role: "tabpanel", "aria-labelledby": `${tab.desc.id}-tab` },
+        React.createElement("div", { id: "ordino-start-menu", className: `ordino-start-menu tab-content ${props.activeTab ? 'ordino-start-menu-open' : ''} ${props.mode === EStartMenuMode.OVERLAY ? 'ordino-start-menu-overlay' : ''}` }, props.tabs.map((tab) => (React.createElement("div", { className: `tab-pane fade ${props.activeTab === tab ? `active show` : ''} ${props.mode === EStartMenuMode.START ? `pt-5` : ''}`, key: tab.desc.id, id: tab.desc.id, role: "tabpanel", "aria-labelledby": `${tab.desc.id}-tab` },
             props.mode === EStartMenuMode.OVERLAY &&
                 React.createElement("div", { className: "container-fluid" },
                     React.createElement("div", { className: "row" },

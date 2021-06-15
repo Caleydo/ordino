48dabfaafa9dfdfb2ff8c56a539650c1b3ae2ca4
Holger Stitz <holger.stitz@datavisyn.io>
2021-06-15 20:41:20 +0200

    Rename `.targid` to `.filmstrip` to avoid confusion

--- src/internal/OrdinoApp.tsx
+++ src/internal/OrdinoApp.tsx
@@ -518,7 +518,7 @@ export class OrdinoApp extends React.Component<IOrdinoAppProps, IOrdinoAppState>
             <StartMenuComponent header={this.props.header} mode={this.state.mode} open={this.state.open}></StartMenuComponent>
             <OrdinoBreadcrumbs views={this.state.views} onClick={(view) => this.showInFocus(view)}></OrdinoBreadcrumbs>
             <div className="wrapper">
-              <div className="targid" ref={this.nodeRef}>{/* ViewWrapper will be rendered as child elements here */}</div>
+              <div className="filmstrip" ref={this.nodeRef}>{/* ViewWrapper will be rendered as child elements here */}</div>
             </div>
           </OrdinoContext.Provider>
         </GraphContext.Provider>

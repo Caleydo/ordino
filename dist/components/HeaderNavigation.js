c038a7d7e9ae38a275cd7e5c6eb5c95e283199b8
Holger Stitz <holger.stitz@datavisyn.io>
2021-09-01 13:34:04 +0200

    Fix data-bs-* attributes for navbar toggle

--- dist/components/HeaderNavigation.js
+++ dist/components/HeaderNavigation.js
@@ -7,7 +7,7 @@ export function HeaderNavigation({ links, fixed, bg = 'dark' }) {
             React.createElement("a", { href: "#/", className: "navbar-brand" },
                 React.createElement(OrdinoLogo, null)),
             React.createElement("a", { href: "/app/", className: "order-2 mx-3 mx-lg-0 ms-auto ms-lg-3 btn btn-light" }, "Start Analysis"),
-            React.createElement("button", { className: "navbar-toggler", type: "button", "data-toggle": "collapse", "data-target": "#ordino-header-navbar-nav", "aria-controls": "ordino-header-navbar-nav", "aria-expanded": "false", "aria-label": "Toggle navigation" },
+            React.createElement("button", { className: "navbar-toggler", type: "button", "data-bs-toggle": "collapse", "data-bs-target": "#ordino-header-navbar-nav", "aria-controls": "ordino-header-navbar-nav", "aria-expanded": "false", "aria-label": "Toggle navigation" },
                 React.createElement("span", { className: "navbar-toggler-icon" })),
             React.createElement("div", { className: "order-1 navbar-collapse collapse", id: "ordino-header-navbar-nav" }, links &&
                 React.createElement("ul", { className: "navbar-nav" }, links.map(({ text, page, faIcon }) => React.createElement("li", { className: "px-3 nav-item" },

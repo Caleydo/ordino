648437461d533ecff3cc3efbba2dc4324bce7fc9
Holger Stitz <holger.stitz@datavisyn.io>
2021-10-05 16:08:41 +0200

    Create Ordino state and context

--- dist/app/OrdinoComponent.js
+++ dist/app/OrdinoComponent.js
@@ -1,5 +1,14 @@
 import * as React from "react";
+const defaultState = {
+    views: {},
+    focusViewId: null
+};
+const OrdinoContext = React.createContext({
+    state: defaultState,
+    setState: () => { }
+});
 export const OrdinoComponent = (props) => {
+    const [appState, setAppState] = React.useState(defaultState);
     React.useEffect(() => {
         let mounted = true;
         // Allow the object refs to be created before replaying the session
@@ -13,9 +22,10 @@ export const OrdinoComponent = (props) => {
             mounted = false;
         };
     }, []);
-    return (React.createElement("div", { className: "ordinoApp" },
-        React.createElement("div", null, "Navbar"),
-        React.createElement("div", null, "Breadcrumbs"),
-        React.createElement("div", null, "Filmstrip")));
+    return (React.createElement(OrdinoContext.Provider, { value: { state: appState, setState: setAppState } },
+        React.createElement("div", { className: "ordinoApp" },
+            React.createElement("div", null, "Navbar"),
+            React.createElement("div", null, "Breadcrumbs"),
+            React.createElement("div", null, "Filmstrip"))));
 };
 //# sourceMappingURL=OrdinoComponent.js.map

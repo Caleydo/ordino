297aacc04e636731737c85f83c971931232d2441
oltionchampari <oltion.champari@datavisyn.io>
2021-11-10 16:02:14 +0100

    Do not reload session when persisting it

--- src/internal/components/session/CurrentSessionCard.tsx
+++ src/internal/components/session/CurrentSessionCard.tsx
@@ -1,6 +1,6 @@
-import {I18nextManager} from 'phovea_core';
+import {GlobalEventHandler, I18nextManager, PropertyHandler, ProvenanceGraph} from 'phovea_core';
 import React from 'react';
-import {ProvenanceGraphMenuUtils} from 'tdp_core';
+import {ErrorAlertHandler, NotificationHandler, ProvenanceGraphMenuUtils} from 'tdp_core';
 import {IStartMenuSessionSectionDesc} from '../../..';
 import {GraphContext, HighlightSessionCardContext} from '../../OrdinoApp';
 import {ListItemDropdown} from '../../../components';
@@ -9,20 +9,48 @@ import {SessionListItem} from './SessionListItem';
 
 
 export default function CurrentSessionCard({name, faIcon}: IStartMenuSessionSectionDesc) {
-    const {graph} = React.useContext(GraphContext);
+    const {manager, graph} = React.useContext(GraphContext);
     const {highlight, setHighlight} = React.useContext(HighlightSessionCardContext);
-    const desc = graph.desc;
+    const [desc, setDesc] = React.useState(graph.desc)
 
     const onHighlightAnimationEnd = () => {
         setHighlight(false);
     };
 
+    const saveCurrentSession = (event: React.MouseEvent, graph: ProvenanceGraph) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (ProvenanceGraphMenuUtils.isPersistent(graph.desc)) {
+            return false;
+        }
+        ProvenanceGraphMenuUtils.persistProvenanceGraphMetaData(graph.desc).then((extras: any) => {
+            console.log('extra', graph, extras)
+            if (extras !== null) {
+                Promise.resolve(manager.migrateGraph(graph, extras)).catch(ErrorAlertHandler.getInstance().errorAlert).then(() => {
+                    setDesc(graph.desc)
+                    const p = new PropertyHandler(location.hash);
+                    const hash = new Map<string, string>();
+                    p.forEach((key, value) => {
+                        hash.set(key, `${encodeURIComponent(key)}=${encodeURIComponent(value)}`);
+                    });
+                    hash.set('clue_graph', `clue_graph=${encodeURIComponent(graph.desc.id)}`);
+                    hash.set('clue_state', `clue_state=${graph.act.id}`);
+                    const url = `${location.href.replace(location.hash, '')}#${Array.from(hash.values()).join('&')}`;
+                    NotificationHandler.pushNotification('success', `${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.successNotification', {name: graph.desc.name})}
+              <br>${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.urlToShare')} <br>
+              <a href="${url}" title="${I18nextManager.getInstance().i18n.t('tdp:core.EditProvenanceMenu.currentLink')}">${url}</a>`, -1);
+                    GlobalEventHandler.getInstance().fire(ProvenanceGraphMenuUtils.GLOBAL_EVENT_MANIPULATED);
+                });
+            }
+        });
+        return false;
+    }
+
     return (
         <CommonSessionCard cardName={name} highlight={highlight} onHighlightAnimationEnd={onHighlightAnimationEnd} faIcon={faIcon} cardInfo={I18nextManager.getInstance().i18n.t('tdp:ordino.startMenu.currentCardInfo')}>
             {(sessionAction) => {
-                const disabled = ProvenanceGraphMenuUtils.isPersistent(desc);
                 return <SessionListItem desc={desc} selectSession={(event) => sessionAction(EAction.SELECT, event, desc)}>
-                    <button type="button" className="me-2 pt-1 pb-1 btn btn-outline-secondary" title={I18nextManager.getInstance().i18n.t('tdp:ordino.startMenu.saveSession')} disabled={disabled} onClick={(event) => sessionAction(EAction.SAVE, event, desc)}>
+                    <button type="button" className="me-2 pt-1 pb-1 btn btn-outline-secondary" title={I18nextManager.getInstance().i18n.t('tdp:ordino.startMenu.saveSession')} disabled={ProvenanceGraphMenuUtils.isPersistent(desc)} onClick={(event) => saveCurrentSession(event, graph)}>
                         {I18nextManager.getInstance().i18n.t('tdp:ordino.startMenu.save')}
                     </button>
                     <ListItemDropdown>

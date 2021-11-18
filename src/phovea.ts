a1b2ea42265586a39ee0268898af9d90d28ba9a5
oltionchampari <oltion.champari@datavisyn.io>
2021-11-18 22:21:53 +0100

    Fix missing logos

--- src/phovea.ts
+++ src/phovea.ts
@@ -9,11 +9,11 @@ import {EP_ORDINO_LOGO, IOrdinoLogoDesc} from './base';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   // generator-phovea:begin
-  registry.push(EP_ORDINO_LOGO, 'ordino_logo', () => import('ordino/dist/assets/logos/ordino.svg').then(PluginRegistry.getInstance().asResource), <IOrdinoLogoDesc>{
+  registry.push(EP_ORDINO_LOGO, 'ordino_logo', () => import('tdp_core/dist/assets/icons/datavisyn_logo_blue_white.svg').then(PluginRegistry.getInstance().asResource), <IOrdinoLogoDesc>{
     text: 'Ordino',
-    width: 30,
-    height: 30
-  });
+    width: 24,
+    height: 24
+});
   // generator-phovea:end
 
 }
--- src/visyn/headerComponents/LoginMenu.tsx
+++ src/visyn/headerComponents/LoginMenu.tsx
@@ -9,7 +9,7 @@ export function LoginMenu({username = 'admin'}: ILoginMenuProps) {
     return <li className="nav-item dropdown" id="user_menu">
         <a href="#" className="nav-link" data-bs-toggle="dropdown" role="button" aria-haspopup="true" id="userMenuDropdown" aria-expanded="false">
             <i className="fas fa-user" aria-hidden="true"></i>
-            <span className="ms-1">jovial_banach</span></a>
+            <span className="ms-1">{username}</span></a>
         <div className="dropdown-menu dropdown-menu-end" aria-labelledby="userMenuDropdown">
             <a className="dropdown-item" href="#" id="logout_link">Logout</a>
         </div>

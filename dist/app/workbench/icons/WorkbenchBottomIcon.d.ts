9e642e6f9da78cac1925aa1fe4a2126a96a21003
oltionchampari <oltion.champari@datavisyn.io>
2022-03-21 11:59:05 +0100

    Remove deleted dist files

--- dist/app/workbench/icons/WorkbenchBottomIcon.d.ts
+++
@@ -1,6 +0,0 @@
-import { IWorkbenchView } from '../../..';
-export interface IWorkbenchIconProps {
-    view: IWorkbenchView;
-}
-export declare function WorkbenchBottomIcon({ view }: IWorkbenchIconProps): JSX.Element;
-//# sourceMappingURL=WorkbenchBottomIcon.d.ts.map
--- dist/app/workbench/useLoadWorkbenchViewPlugin.js
+++
@@ -1,9 +0,0 @@
-import { PluginRegistry, useAsync, EXTENSION_POINT_VISYN_VIEW } from 'tdp_core';
-export function useVisynViewPlugin(index, viewId) {
-    const view = PluginRegistry.getInstance().getVisynPlugin(EXTENSION_POINT_VISYN_VIEW, viewId);
-    // TODO:: Handle view being null
-    const { status, value: viewPlugin } = useAsync(view.load, []);
-    // TODO:: need to make this typesafe at some point
-    return viewPlugin ? { ...viewPlugin } : null;
-}
-//# sourceMappingURL=useLoadWorkbenchViewPlugin.js.map

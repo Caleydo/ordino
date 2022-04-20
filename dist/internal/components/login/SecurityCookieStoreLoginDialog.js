0cf43e691c83d48d283b4619a99b532465f79b07
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2022-04-20 10:44:56 +0200

    integrate feedback

--- dist/internal/components/login/SecurityCookieStoreLoginDialog.js
+++ dist/internal/components/login/SecurityCookieStoreLoginDialog.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { I18nextManager } from 'tdp_core';
-import { useOrdinoLogo } from '../../../hooks/useOrdinoLogo';
 function SecurityCookieStoreLoginDialog() {
-    const { status, value } = useOrdinoLogo();
     return (React.createElement("div", { style: { minHeight: 200, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' } },
-        status === 'success' && React.createElement("img", { src: value.icon, style: { width: '60px', marginBottom: '2em' }, alt: value.text }),
         I18nextManager.getInstance().i18n.t('tdp:ordino.loginMenu.automaticLoginDisclaimer'),
         React.createElement("button", { className: "btn btn-outline-secondary btn-sm mt-2", type: "button", onClick: () => window.location.reload() }, I18nextManager.getInstance().i18n.t('tdp:ordino.loginMenu.refreshPage'))));
 }

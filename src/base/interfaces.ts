0b9fad96a6f9fd32479d061574138a1e0ea865d1
Christian Bors <christian.bors@datavisyn.io>
2022-04-26 13:48:50 +0200

    address PR comments

--- src/base/interfaces.ts
+++ src/base/interfaces.ts
@@ -4,14 +4,17 @@ export interface IRelationEntityReference {
   columns: string[];
 }
 
-// TODO: Refactor
+// TODO: This enum was moved from reprovisyn
+// It requires further refactoring together with lineup and tdp code. See links below for more details
+// tdp_core https://github.com/datavisyn/tdp_core/blob/0548b2c45def8db6dc8a113daf34cd6d0be68503/src/base/rest.ts#L121
+// lineup usages: https://github.com/datavisyn/tdp_core/blob/4bfd793c3b139a582031b16e987b07d1d1689ce2/src/lineup/desc.ts#L98
 export enum EColumnType {
   categorical = 'categorical',
   number = 'number',
   string = 'string',
 }
 
-// TODO: Refactor this
+// TODO: This interface was moved from Reprovisyn and needs further refactoring together with the lineup/tdp_core column interfaces
 export interface IColumnReference {
   columnName: string;
   show: boolean;

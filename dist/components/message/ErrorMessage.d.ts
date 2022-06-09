009ea416efbc9c73a48183b043fc5f845377603d
Christian Bors <christian.bors@datavisyn.io>
2022-06-09 13:58:13 +0200

    add type response to error message

--- dist/components/message/ErrorMessage.d.ts
+++ dist/components/message/ErrorMessage.d.ts
@@ -1,5 +1,5 @@
 export declare function ErrorMessage({ error, onRetry }: {
-    error: Error | string | null;
+    error: Error | Response | string | null;
     onRetry?: () => void;
 }): JSX.Element;
 //# sourceMappingURL=ErrorMessage.d.ts.map
--- dist/components/message/ErrorMessage.js
+++ dist/components/message/ErrorMessage.js
@@ -3,7 +3,7 @@ import { I18nextManager } from 'tdp_core';
 export function ErrorMessage({ error, onRetry }) {
     return error ? (React.createElement("div", { className: "alert alert-danger flex-fill d-flex align-items-center", role: "alert" },
         React.createElement("i", { className: "fas fa-fw fa-exclamation" }),
-        React.createElement("div", { className: "flex-fill" }, error.toString()),
+        React.createElement("div", { className: "flex-fill" }, error instanceof Response ? error.statusText : error.toString()),
         onRetry ? (React.createElement("button", { type: "button", className: "btn btn-sm btn-outline-danger", onClick: onRetry }, I18nextManager.getInstance().i18n.t('tdp:ordino.message.retry'))) : null)) : null;
 }
 //# sourceMappingURL=ErrorMessage.js.map

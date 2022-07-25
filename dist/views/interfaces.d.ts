21fd2555d77e33b5a0fb1c67b38dd5b363b1f26d
Holger Stitz <holger.stitz@datavisyn.io>
2022-07-25 15:06:54 +0200

    Add Ordino-specific view plugin desc
    * `OrdinoSimpleViewPluginType` extends the `VisynSimpleViewPluginType` (e.g., for proxy views)
    * `OrdinoDataViewPluginType` extends the `VisynDataViewPluginType` (e.g., for general vis view)
    * Add `icon` property to plugin desc

--- dist/views/interfaces.d.ts
+++ dist/views/interfaces.d.ts
@@ -1,9 +1,49 @@
 /// <reference types="react" />
 import { IColumnDesc } from 'lineupjs';
-import { DefineVisynViewPlugin, IScoreRow, IServerColumn } from 'tdp_core';
+import { DefineVisynViewPlugin, IScoreRow, IServerColumn, VisynDataViewPluginType, VisynSimpleViewPluginType } from 'tdp_core';
 import { IOrdinoRelation } from '../base';
 import { ISelectedMapping, IWorkbench } from '../store';
 export interface IOrdinoVisynViewDesc {
+    /**
+     * Name of the view.
+     * Used as label for the view chooser and as title of the the workbench window.
+     */
+    name: string;
+    /**
+     * Description of the view
+     */
+    description?: string;
+    /**
+     * Make the view the default view of a workbench.
+     * Should only be applied to a single view.
+     */
+    defaultView?: boolean;
+    /**
+     * Font Awesome icon (e.g., `far fa-table`).
+     * Optional icon for each visyn view in Ordino.
+     * @default "far fa-window-maximize"
+     */
+    icon?: string;
+}
+/**
+ * Extend `VisynSimpleViewPluginType` with `IOrdinoVisynViewDesc`
+ * to enable type checking and auto-complete for the plugin desc
+ * when registering the views.
+ */
+export declare type OrdinoSimpleViewPluginType<Param extends Record<string, unknown> = Record<string, unknown>, Desc extends Record<string, unknown> = Record<string, unknown>> = VisynSimpleViewPluginType<Param, Desc & IOrdinoVisynViewDesc>;
+/**
+ * Extend `OrdinoDataViewPluginType` with `IOrdinoVisynViewDesc`
+ * to enable type checking and auto-complete for the plugin desc
+ * when registering the views.
+ */
+export declare type OrdinoDataViewPluginType<Param extends Record<string, unknown> = Record<string, unknown>, Desc extends Record<string, unknown> = Record<string, unknown>> = VisynDataViewPluginType<Param, Desc & IOrdinoVisynViewDesc>;
+/**
+ * Plugin desc for the Ordino ranking view
+ */
+export interface IOrdinoRankingViewDesc extends IOrdinoVisynViewDesc {
+    /**
+     * Relation for the Ordino view
+     */
     relation: IOrdinoRelation;
 }
 export interface IOrdinoRankingViewParam {
@@ -55,11 +95,11 @@ export declare type OrdinoRankingViewPluginType<Param extends Record<string, unk
      * @param formatting TODO add typings once Ollie's interface refactoring is finished
      */
     onAddFormatting(formatting: IWorkbench['formatting']): void;
-}, Desc & IOrdinoVisynViewDesc>;
-export declare type OrdinoVisynViewPluginDesc = OrdinoRankingViewPluginType['desc'];
-export declare type OrdinoVisynViewPlugin = OrdinoRankingViewPluginType['plugin'];
-export declare function isVisynRankingViewDesc(desc: unknown): desc is OrdinoVisynViewPluginDesc;
-export declare function isVisynRankingView(plugin: unknown): plugin is OrdinoVisynViewPlugin;
+}, Desc & IOrdinoRankingViewDesc>;
+export declare type OrdinoRankingViewPluginDesc = OrdinoRankingViewPluginType['desc'];
+export declare type OrdinoRankingViewPlugin = OrdinoRankingViewPluginType['plugin'];
+export declare function isVisynRankingViewDesc(desc: unknown): desc is OrdinoRankingViewPluginDesc;
+export declare function isVisynRankingView(plugin: unknown): plugin is OrdinoRankingViewPlugin;
 /**
  * Find all available workbenches to transition to for my workbench
  * @param idType
@@ -71,7 +111,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
             readonly [key: string]: any;
             visynViewType: "ranking";
             defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-        } & Record<string, unknown> & IOrdinoVisynViewDesc;
+        } & Record<string, unknown> & IOrdinoRankingViewDesc;
         viewType: "ranking";
         defaultParameters: Record<string, unknown> & IOrdinoRankingViewParam;
         factory: never;
@@ -124,7 +164,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
@@ -178,7 +218,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
@@ -233,7 +273,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
@@ -287,17 +327,13 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
             onSelectionChanged(selection: import("react").SetStateAction<string[]>): void;
             onParametersChanged(parameters: import("react").SetStateAction<Record<string, unknown> & IOrdinoRankingViewParam>): void;
         }>>;
-        /**
-         * Callback when the Column Description changed.
-         * @param desc New Column Description.
-         */
         tab?: import("react").ComponentType<{
             /**
              * Data array matching the columns defined in the `columnDesc`.
@@ -346,7 +382,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
@@ -400,7 +436,7 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
                 readonly [key: string]: any;
                 visynViewType: "ranking";
                 defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-            } & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc;
+            } & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc;
         } & {
             selection: string[];
             parameters: Record<string, unknown> & IOrdinoRankingViewParam;
@@ -412,5 +448,5 @@ export declare const findWorkbenchTransitions: (idType: string) => Promise<({
     readonly [key: string]: any;
     visynViewType: "ranking";
     defaultParameters?: Record<string, unknown> & IOrdinoRankingViewParam;
-} & Record<string, unknown> & IOrdinoVisynViewDesc & import("tdp_core").IPluginDesc)[]>;
+} & Record<string, unknown> & IOrdinoRankingViewDesc & import("tdp_core").IPluginDesc)[]>;
 //# sourceMappingURL=interfaces.d.ts.map

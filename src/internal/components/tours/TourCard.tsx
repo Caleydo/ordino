c0c236e8c579064de71571b50c4404b724808083
dvmichaelpeterseil <109951727+dvmichaelpeterseil@users.noreply.github.com>
2023-04-11 14:13:38 +0200

    fix: add vertical padding to tour cards and use full height teaser image (#430)
    Co-authored-by: Holger Stitz <holger.stitz@datavisyn.io>

--- src/internal/components/tours/TourCard.tsx
+++ src/internal/components/tours/TourCard.tsx
@@ -12,9 +12,9 @@ interface ITourCardProps {
 
 export function TourCard({ id, image, title, text, onClickHandler, href }: ITourCardProps) {
   return (
-    <div className="col position-relative" data-testid="tour-card">
+    <div className="mb-3 col position-relative" data-testid="tour-card">
       <div className="card ordino-tour-card shadow-sm" data-id={id} data-testid={id}>
-        {image ? <img className="card-img-top p-2" style={{ height: '200px' }} src={image} alt="Tour Image" /> : null}
+        {image ? <img className="card-img-top p-2" src={image} alt="Teaser image of the tour" /> : null}
         <div className="card-body p-2">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{text}</p>
